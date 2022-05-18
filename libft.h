340201ee822e6a8f1c8d40968861e63643e6b924
Thong Zi qi <tzi-qi@u82z04s02.42kl.edu.my>
2022-05-18 22:07:04 +0800

    yet maybe idk how to use header file

--- libft.h
+++ libft.h
@@ -3,12 +3,12 @@
 
 #include <unistd.h>
 
-int 	ft_isalnum(int c);
-int 	ft_isalpha(char c);
-int		ft_isascii(unsigned int a);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
+int	ft_isalnum(int c);
+int	ft_isalpha(char c);
+int	ft_isascii(unsigned int a);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
 void	*ft_memset(void *str, int c, size_t n);
-int		ft_strlen(char *str);
+int	ft_strlen(char *str);
 
 #endif
